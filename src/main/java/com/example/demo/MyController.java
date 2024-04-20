f6cbdab0c6241f127c4e369fe410871bbefbea22
dsubnaik <158210400+dsubnaik@users.noreply.github.com>
2024-04-19 21:51:19 -0500

    add comments to MyController.java

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,15 +1,27 @@
+//************************************
+//Program Name: ChatController.java
+//Developer: XChange
+//Date Created: 04/19/2024
+//Version: 1.0
+//Purpose: allows hello page to work
+//************************************
+
 package com.example.demo;
 
+//imports
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//MyController class
 @Controller
 public class MyController {
 
+    //hello method
     @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("message", "Hello, this is generated using Thymeleaf!");
         return "hello";
-    }
-}
+    }//end of hello method
+    
+}//end of MyController
