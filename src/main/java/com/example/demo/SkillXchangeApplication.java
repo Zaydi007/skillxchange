a470e383843df1ff3b24cebf70116700a5545834
dsubnaik <158210400+dsubnaik@users.noreply.github.com>
2024-04-19 22:07:21 -0500

    add comments to SkillXchangeApplication.java

--- src/main/java/com/example/demo/SkillXchangeApplication.java
+++ src/main/java/com/example/demo/SkillXchangeApplication.java
@@ -1,8 +1,16 @@
+//************************************
+//Program Name: SkillXChangeApplication.java
+//Developer: XChange
+//Date Created: 04/19/2024
+//Version: 1.0
+//Purpose: allows application to run
+//************************************
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SkillXChangeApplication
 @SpringBootApplication
 public class SkillXchangeApplication {
 
@@ -10,4 +18,4 @@ public class SkillXchangeApplication {
 		SpringApplication.run(SkillXchangeApplication.class, args);
 	}
 
-}
+}//end of SkillXChangeApplication
